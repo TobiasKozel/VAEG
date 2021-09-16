5072940f66d6f8517367e91451e74686361bbaba
Tobias Kozel <t.kozel@protonmail.com>
2021-09-16 02:05:01 +0200

    mono samples

--- demo/main.gd
+++ demo/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var time: float = 0.0
+
 func _ready():
 	pass # Replace with function body.
 
@@ -8,3 +10,8 @@ func _input(event):
 		var key: InputEventKey = event
 		if key.scancode == KEY_ESCAPE:
 			get_tree().quit()
+
+
+func _process(delta):
+	time += delta
+	$Emitter.translation.x = sin(time * 5) * 4
--- src/vaeg_emitter.hpp
+++ src/vaeg_emitter.hpp
@@ -10,6 +10,7 @@ namespace vaeg {
 	godot_method_bind* gdn_bind_get_rid;
 	godot_method_bind* gdn_bind_get_id;
 	godot_method_bind* gdn_bind_get_translation;
+	godot_method_bind* gdn_bind_is_stereo;
 
 	struct Emitter {
 		tklb::Handle handle;
@@ -41,10 +42,12 @@ namespace vaeg {
 				"AudioStreamSample"
 			)
 			VAEG_REGISTER_METHOD(_process)
-			VAEG_REGISTER_METHOD(_init)
+			VAEG_REGISTER_METHOD(_ready)
 
+			gdn_bind_is_stereo =       api->godot_method_bind_get_method("AudioStreamSample", "is_stereo");
 			gdn_bind_get_buffer_data = api->godot_method_bind_get_method("AudioStreamSample", "get_data");
 			gdn_bind_get_translation = api->godot_method_bind_get_method("Spatial", "get_translation");
+
 		}
 
 		void updateTransform(double delta = 0) {
@@ -56,8 +59,9 @@ namespace vaeg {
 			emitter->velocity = emitter->position - old;
 		}
 
-		VAEG_FUNC(_init, int num_args, godot_variant** args) {
+		VAEG_FUNC(_ready, int num_args, godot_variant** args) {
 			updateTransform();
+			emitter->state[vae::core::Emitter::ready] = true;
 			godot_variant ret;
 			api->godot_variant_new_nil(&ret);
 			return ret;
@@ -80,14 +84,16 @@ namespace vaeg {
 			const void* no_args[1] = { };
 			api->godot_method_bind_ptrcall(gdn_bind_get_buffer_data, parameter, no_args, &pool);
 			godot_int size = api->godot_pool_byte_array_size(&pool);
-			const int channels = 2;
+			godot_bool stereo = true;
+			api->godot_method_bind_ptrcall(gdn_bind_is_stereo, parameter, no_args, &stereo);
+			const int channels = stereo ? 2 : 1;
 			const int length = size / (channels * 2);
 			godot_pool_byte_array_read_access* read_access = api->godot_pool_byte_array_read(&pool);
 			const uint8_t* data = api->godot_pool_byte_array_read_access_ptr(read_access);
 
 			tklb::Handle h = Engine::instance().createClip();
 			auto clip = Engine::instance().getClip(h);
-			clip->data.resize(length, 2);
+			clip->data.resize(length, channels);
 			clip->data.setFromInterleaved(reinterpret_cast<const short*>(data), length, channels);
 			clip->data.multiply(1.0 / 32768.0);
 			emitter->clip = h;
@@ -96,6 +102,11 @@ namespace vaeg {
 			api->godot_pool_byte_array_destroy(&pool);
 		}
 
+		/**
+		 * This should return a ref object
+		 * but since we don't need to get data back
+		 * just return nil
+		 */
 		VAEG_GET(sample_ref) {
 			godot_variant ret;
 			api->godot_variant_new_nil(&ret);
