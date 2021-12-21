aabe2a6639d1a121409777fe9b3036f51373a737
Tobias Kozel <t.kozel@protonmail.com>
2021-12-21 21:28:35 +0100

    m

--- demo/Levels/Main/L_Main.gd
+++ demo/Levels/Main/L_Main.gd
@@ -7,13 +7,13 @@ func engine_started():
 
 func _ready() -> void:
 	EngineInstance.connect("vae_started", self, "engine_started")
-	EngineInstance.load_hrtf("hrtf.msgpack")
+	EngineInstance.load_hrtf("hrtf.json")
 	EngineInstance.load_bank("bank1")
 	EngineInstance.start()
-	
+
 	if !OS.is_debug_build():
 		fast_close = false
-	
+
 	if fast_close:
 		print("** Fast Close enabled in the 'L_Main.gd' script **")
 		print("** 'Esc' to close 'Shift + F1' to release mouse **")
@@ -24,7 +24,7 @@ func _input(event: InputEvent) -> void:
 		$ArenaGeometry/VAEEmitter.play()
 	if event.is_action_pressed("ui_cancel") and fast_close:
 		get_tree().quit() # Quits the game
-	
+
 	if event.is_action_pressed("mouse_input") and fast_close:
 		match Input.get_mouse_mode():
 			Input.MOUSE_MODE_CAPTURED:
