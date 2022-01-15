6aa10e486bc5b726dac81b9a6b1cd558282b3216
Tobias Kozel <t.kozel@protonmail.com>
2022-01-16 00:29:00 +0100

    m

--- demo/Levels/Main/ReverbZone.gd
+++ demo/Levels/Main/ReverbZone.gd
@@ -3,19 +3,6 @@ extends Area
 export(int) var bank = 0
 export(int) var mixer = 0
 export(int) var effect = 0
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_ReverbZone_body_entered(body):
@@ -24,12 +11,11 @@ func _on_ReverbZone_body_entered(body):
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 1, 0.3)
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 2, 1.0)
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 3, 0.1)
-		print("To small")
+
 
 
 func _on_ReverbZone_body_exited(body):
 	if body.is_in_group("Player"):
-		print("To normal")
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 0, 0.3) # fb1 
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 1, 0.6) # fb2
 		EngineInstance.set_mixer_effect(bank, mixer, effect, 2, 0.4) # mix
